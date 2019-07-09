92fa1924dbbb09ee53b28b604bf73ab1eec63fb7
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 19:59:20 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,18 +1,18 @@
 def first_challenge
    epic_tragedy = {
      :montague => {
-       :patriarch => {
+#       :patriarch => {
 #          :name => "Lord Montague", :age => "53"
-          },
-     :matriarch => {
+#          },
+#     :matriarch => {
 #       :name => "Lady Montague", :age => "54"
-         },
-     :hero => {
+#         },
+#     :hero => {
 #       :name => "Romeo", :age => "15", :status => "alive"
-       }, 
-     :hero_friends => [
+#       }, 
+#     :hero_friends => [
 #       {:name => "Benvolio", :age => "17", :attitude =>"worried"},{:name => "Mercutio", :age => "18", :attitude =>"hot-headed"}
-       ]
+#       ]
      },
    :capulet => {
  #    :patriarch => {:name => "Lord Capulet", :age => "50"},:matriarch => {:name => "Lady Capulet", :age => "51"}, :heroine => {:name => "Juliet", :age => "15", :status => "alive"}, :heroine_friends => [{:name => "Steven", :age => "30", :attitude =>"confused"},{:name => "Nurse", :age => "44", :attitude =>"worried"}]
