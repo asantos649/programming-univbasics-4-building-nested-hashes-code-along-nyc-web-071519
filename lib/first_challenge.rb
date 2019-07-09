73f5401999c01dbb98cf1cab6a4babca4696a2ac
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 19:34:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,4 +1,4 @@
 def first_challenge
    epic_tragedy = {:montague => {:patriarch => {},:matriarch => {}, :hero => {}, :hero_friends => []},
-   :capulet => {}}
+   :capulet => {:patriarch => {},:matriarch => {}, :heroine => {}, :heroine_friends => []}}
 end
