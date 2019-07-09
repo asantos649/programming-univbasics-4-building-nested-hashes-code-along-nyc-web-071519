4a36d2c3e72966a03ea0248341e4fca62080c0dc
Andrew Santos <asantos@alumni.nd.edu>
2019-07-09 19:36:56 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,4 +1,4 @@
 def first_challenge
-   epic_tragedy = {:montague => {:patriarch => {},:matriarch => {}, :hero => {}, :hero_friends => []},
+   epic_tragedy = {:montague => {:patriarch => {:name => "Lord Montague", :age => "53"},:matriarch => {:name => "Lady Montague", :age => "54"}, :hero => {:name => "Romeo", :age => "53"}, :hero_friends => []},
    :capulet => {:patriarch => {},:matriarch => {}, :heroine => {}, :heroine_friends => []}}
 end
